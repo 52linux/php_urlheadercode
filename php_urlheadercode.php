c4e952a8853a8b4c3c365ae1d55ebc86368582e3
zjpro <52linux@users.noreply.github.com>
2019-01-05 14:22:29 +0800

    Create php_urlheadercode.php

---
+++ php_urlheadercode.php
@@ -0,0 +1,22 @@
+<?php
+
+function get302($url)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_VERBOSE, 0);
+	curl_setopt($ch, CURLOPT_HEADER, true);
+	curl_setopt($ch, CURLOPT_NOBODY, true);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+	$ret = curl_exec($ch);
+	$info = curl_getinfo($ch);
+	$http_code = $info['http_code'];
+	curl_close($ch);
+	return $http_code;
+}
